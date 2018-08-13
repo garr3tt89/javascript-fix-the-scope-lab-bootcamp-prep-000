d43bb13a6582c8c65f4b55c0b852af7e6fd811fe
garr3tt1989@gmail.com Hausman <garr3tt1989@gmail.com>
2018-08-13 19:05:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,17 @@ function yourAnimal() {
   var animal = 'cat';
   return animal;
 }
+
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
   const two = 2
+}
+
+function add2(n) {
+ var two = 2
+  return n + two;
 }
 
 var funkyFunction = function() {
